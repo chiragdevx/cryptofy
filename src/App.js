8674175592e2f3636f8140b04639a1153515793e
Chirag-parmarr <chiragparmar13913@gmail.com>
2022-07-06 22:13:03 +0530

    feat:create some components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { makeStyles } from "@material-ui/core";
-// import Homepage from "./Pages/HomePage";
+import Homepage from "./Pages/HomePage";
 import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
-// import CoinPage from "./Pages/CoinPage";
+import CoinPage from "./Pages/CoinPage";
 import Header from "./components/Header";
 
 const useStyles = makeStyles(() => ({
@@ -20,8 +20,8 @@ function App() {
     <BrowserRouter>
       <div className={classes.App}>
         <Header />
-        {/* <Route path="/" component={Homepage} exact />
-        <Route path="/coins/:id" component={CoinPage} exact /> */}
+        <Route path="/" component={Homepage} exact />
+        <Route path="/coins/:id" component={CoinPage} exact />
       </div>
     </BrowserRouter>
   );
